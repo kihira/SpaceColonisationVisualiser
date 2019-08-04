f0408b15bf2ffaa4122620eeceaf1dad8c3d5d20
Zoe <zoe@kihira.uk>
2019-08-04 12:16:56 +0100

    Change background to make it more visible and clean up code a tad

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,25 @@
-import {PerspectiveCamera, Scene, Vector3, WebGLRenderer} from "three";
+import {Color, PerspectiveCamera, Plane, PlaneHelper, Scene, Vector3, WebGLRenderer} from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import Tree from "./Tree";
 
-const scene = new Scene();
-const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+// Init renderer
 const renderer = new WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setPixelRatio( window.devicePixelRatio );
 document.body.appendChild( renderer.domElement );
 
-const controls = new OrbitControls(camera, renderer.domElement);
-
+// Init camera
+const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.set(0, 0, 10);
 camera.lookAt(0, 0, 0);
+
+// Init controls
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
+
+// Init scene
+const scene = new Scene();
+scene.background = new Color(0xEEEEEE);
 
 // Create a tree
 const tree = new Tree({
@@ -23,14 +28,18 @@ const tree = new Tree({
     branchThickness: 0.2,
     crownCentre: new Vector3(0, 2.5, 0),
     crownSize: new Vector3(2, 5, 2),
-    influenceRadius: 15,
+    influenceRadius: 7,
     killDistance: 2,
     maxIterations: 200,
     nodeSize: 0.15,
 }, new Vector3(0, 0, 0));
-
 tree.generateGeometry(scene);
 
+// Generate ground plane
+const plane = new Plane(new Vector3(0, 1, 0));
+scene.add(new PlaneHelper(plane, 4, 0xAAAAAA));
+
+// Start the animation/render loop
 animate();
 
 function animate() {
